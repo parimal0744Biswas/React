3160307113a812f610e84cea59480b6932af7a7f
PARIMAL BISWAS <biswasparimal01@gmail.com>
2024-01-28 18:00:09 +0530

    .

---
+++ ALL Project/01_basic/src/App.js
@@ -0,0 +1,12 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Yoooooooooooo</h1>
+      <Chai/>
+    </div>
+  );
+}
+
+export default App;
---
+++ ALL Project/01_basic/src/Chai.jsx
@@ -0,0 +1,10 @@
+
+function Chai() {
+    return (
+        <>
+            <h1>Broooooo</h1>
+        </>
+    );
+}
+
+export default Chai;
